37ea0655fba2d9fdd7d079857c40d72944be3950
Gilles Coomans <gilles.coomans@gmail.com>
2017-04-22 23:13:02 +0200

    fix(code): add index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/*
+* @Author: Gilles Coomans
+* @Date:   2017-04-22 23:10:45
+* @Last Modified by:   Gilles Coomans
+* @Last Modified time: 2017-04-22 23:12:20
+*/
+
+import lexiconDef from './ldl-definition';
+import lexicon from './ldl-lexicon';
+import pragmas from './pragmatics/ldl-to-babelute';
+
+export default {
+	lexiconDef,
+	lexicon,
+	pragmas
+};
