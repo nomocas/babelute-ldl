532306d369795f8ac8e4991458388f3b2d660254
Gilles Coomans <gilles.coomans@gmail.com>
2017-04-22 23:16:53 +0200

    fix(definition): clean file

--- src/ldl-definition.js
+++ src/ldl-definition.js
@@ -2,7 +2,8 @@
 * @Author: Gilles Coomans
 * @Date:   2017-04-21 14:50:09
 * @Last Modified by:   Gilles Coomans
-* @Last Modified time: 2017-04-21 14:52:37
+* @Last Modified time: 2017-04-22 23:16:40
 */
 
-'use strict';
+
+export default {};
